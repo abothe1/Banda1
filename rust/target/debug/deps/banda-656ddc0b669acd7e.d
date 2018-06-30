570a1aeb8c53040016c52a4f226e0bf4b1366ecf
sam <sam.westerman@usaepay.com>
2018-06-30 15:18:13 -0700

    testing mysql

--- rust/target/debug/deps/banda-656ddc0b669acd7e.d
+++
@@ -1,5 +0,0 @@
-/Users/sampersand/banda/target/debug/deps/banda-656ddc0b669acd7e: src/main.rs
-
-/Users/sampersand/banda/target/debug/deps/banda-656ddc0b669acd7e.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/deps/banda-789f1f94a4a7bd52.d
+++
@@ -1,5 +0,0 @@
-/Users/sampersand/Banda1/rust/target/debug/deps/banda-789f1f94a4a7bd52: src/main.rs
-
-/Users/sampersand/Banda1/rust/target/debug/deps/banda-789f1f94a4a7bd52.d: src/main.rs
-
-src/main.rs:
